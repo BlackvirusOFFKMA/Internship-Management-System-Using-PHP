cb6293dc97f86d4726e56d84d900b3ded1ea482d
Nghia <emailsub789@gmail.com>
2021-11-21 00:53:12 +0700

    change DB format

--- qlsvtt.sql
+++ qlsvtt.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2021 at 06:22 PM
+-- Generation Time: Nov 20, 2021 at 06:52 PM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,25 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `qlsvtt`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `account`
---
-
-CREATE TABLE `account` (
-  `username` char(10) NOT NULL,
-  `password` int(11) NOT NULL,
-  `role` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `account`
---
-
-INSERT INTO `account` (`username`, `password`, `role`) VALUES
-('AT150734', 123, 2);
 
 -- --------------------------------------------------------
 
@@ -61,16 +42,16 @@ CREATE TABLE `bangdiemtt` (
 
 CREATE TABLE `detai` (
   `MaDT` char(10) NOT NULL,
-  `TenDT` varchar(50) NOT NULL
+  `TenDt` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `giangvienhdtt`
+-- Table structure for table `giaovienhdtt`
 --
 
-CREATE TABLE `giangvienhdtt` (
+CREATE TABLE `giaovienhdtt` (
   `MaGV` char(10) NOT NULL,
   `TenGV` char(50) NOT NULL,
   `GioiTinh` char(3) NOT NULL,
@@ -79,45 +60,6 @@ CREATE TABLE `giangvienhdtt` (
   `Email` varchar(50) NOT NULL,
   `DiaChi` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `khoahoc`
---
-
-CREATE TABLE `khoahoc` (
-  `MaKH` char(10) NOT NULL,
-  `NamBD` date NOT NULL,
-  `NamKT` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `khoahoc`
---
-
-INSERT INTO `khoahoc` (`MaKH`, `NamBD`, `NamKT`) VALUES
-('', '0000-00-00', '0000-00-00'),
-('AT15', '2021-11-01', '2021-11-06');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lop`
---
-
-CREATE TABLE `lop` (
-  `MaLop` char(10) NOT NULL,
-  `TenLop` varchar(30) NOT NULL,
-  `MaKH` char(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `lop`
---
-
-INSERT INTO `lop` (`MaLop`, `TenLop`, `MaKH`) VALUES
-('AT15H', 'AT15', 'AT15');
 
 -- --------------------------------------------------------
 
@@ -134,58 +76,41 @@ CREATE TABLE `nhomtt` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sinhvien`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `sinhvien` (
-  `MaSV` char(10) NOT NULL,
-  `TenSV` varchar(50) NOT NULL,
-  `GioiTinh` char(3) DEFAULT NULL CHECK (`GioiTinh` in ('Nam','Nu')),
-  `NgaySinh` date NOT NULL,
-  `SDT` char(10) NOT NULL,
-  `Email` varchar(30) NOT NULL,
-  `DiaChi` varchar(50) NOT NULL,
-  `MaLop` char(10) NOT NULL
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `date` datetime NOT NULL,
+  `user_id` varchar(60) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `school_id` varchar(60) NOT NULL,
+  `rank` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `sinhvien`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `sinhvien` (`MaSV`, `TenSV`, `GioiTinh`, `NgaySinh`, `SDT`, `Email`, `DiaChi`, `MaLop`) VALUES
-('AT150734', 'le trong nghia', 'nam', '2011-11-01', '0903908285', 't.nghia2605@gmail.com', '7A Cộng Hòa Quận Tân Bình', 'AT15H');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `thongtindt`
---
-
-CREATE TABLE `thongtindt` (
-  `MaDT` char(10) NOT NULL,
-  `MaSV` char(10) NOT NULL,
-  `MaGV` char(10) NOT NULL,
-  `LoaiHinhTT` varchar(15) NOT NULL,
-  `ThoiGianDB` date NOT NULL,
-  `ThoiGianKetThuc` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `date`, `user_id`, `gender`, `school_id`, `rank`, `password`, `image`) VALUES
+(1, 'Eathorne', 'Banda', 'eathorne@yahoo.com', '2021-08-10 19:08:58', 'eathorne.banda', 'male', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'super_admin', '$2y$10$DfpqgNj.g4qKLJCVs9CC5esat5K0jMF49cx6wt4h0B8ZBzw6Ocrci', 'uploads/cardinal_1585485603.jpg'),
+(2, 'Mary', 'Phiri', 'mary@yahoo.com', '2021-08-10 19:49:36', 'mary.phiri', 'female', '', 'super_admin', '$2y$10$QpP3dlDXgmxxv.WdhB1BseUk77iwCHZhu3CcH/RfdcCiHWr3uQmAy', ''),
+(3, 'John', 'Tembo', 'john@yahoo.com', '2021-08-18 14:43:04', 'john.tembo', 'male', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'admin', '$2y$10$tk8F2m/X8g.Ilh432vXggeYBKF7EuyRiKpuXg1atYrup6JxyuezBm', 'uploads/header100people.jpg'),
+(4, 'Anna', 'Jones', 'anna@yahoo.com', '2021-08-18 15:02:29', 'anna.jonnes', 'female', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'reception', '$2y$10$Co6UEpouAV1gFIuTMsB7IuJwZXsQ081kws.61r7GczrjRfDRzrBkW', ''),
+(5, 'Vibe', 'Peters', 'vibe@yahoo.com', '2021-08-18 15:03:07', 'vibe.peters', 'male', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'lecturer', '$2y$10$VzZvMzHH/fIC.MtC0OZxcuYUkTqvA2/PPy42OGSgZt/aDNFmp/rUK', ''),
+(6, 'Bob', 'Marley', 'bob@yahoo.com', '2021-08-18 16:03:55', 'bob.marley', 'male', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'student', '$2y$10$TC9IVVC7ChftDQpUiAJ1NuwKYtLJvuIdsP6BTb4enalnW7h/q4DAi', ''),
+(7, 'Maria', 'Jonnes', 'maria@yahoo.com', '2021-08-18 16:06:27', 'maria.jonnes', 'female', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'student', '$2y$10$4nmgjlK9WIxEswOibyl49elGbdeuKoaP/2hsPuKQdcxt3t3NPlVVe', ''),
+(8, 'Jane', 'Mandawa', 'jane@yahoo.com', '2021-08-18 16:07:00', 'jane.mandawa', 'female', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'student', '$2y$10$deWPI47s4OfH5c/b/cVqeOP3cGT2SQf6G7Vj1VBD6dRId6Lf44fjy', ''),
+(9, 'Guy', 'Dude', 'guy@yahoo.com', '2021-08-21 18:26:48', 'guy.dude', 'male', '0PbzcOAALCLUytlGxNog9R3ZaG5rpvjeleQ3UHSWE81m00vLyqNGBEgK4waH', 'student', '$2y$10$DfpqgNj.g4qKLJCVs9CC5esat5K0jMF49cx6wt4h0B8ZBzw6Ocrci', '');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`username`);
-
---
--- Indexes for table `bangdiemtt`
---
-ALTER TABLE `bangdiemtt`
-  ADD PRIMARY KEY (`MaDT`),
-  ADD KEY `fk_masv_bangdiemtt` (`MaSV`);
 
 --
 -- Indexes for table `detai`
@@ -194,84 +119,34 @@ ALTER TABLE `detai`
   ADD PRIMARY KEY (`MaDT`);
 
 --
--- Indexes for table `giangvienhdtt`
+-- Indexes for table `giaovienhdtt`
 --
-ALTER TABLE `giangvienhdtt`
+ALTER TABLE `giaovienhdtt`
   ADD PRIMARY KEY (`MaGV`);
 
 --
--- Indexes for table `khoahoc`
+-- Indexes for table `users`
 --
-ALTER TABLE `khoahoc`
-  ADD PRIMARY KEY (`MaKH`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `firstname` (`firstname`),
+  ADD KEY `lastname` (`lastname`),
+  ADD KEY `date` (`date`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `school_id` (`school_id`),
+  ADD KEY `gender` (`gender`),
+  ADD KEY `rank` (`rank`),
+  ADD KEY `email` (`email`);
 
 --
--- Indexes for table `lop`
---
-ALTER TABLE `lop`
-  ADD PRIMARY KEY (`MaLop`),
-  ADD KEY `fk_makh` (`MaKH`);
-
---
--- Indexes for table `nhomtt`
---
-ALTER TABLE `nhomtt`
-  ADD PRIMARY KEY (`MaNhom`),
-  ADD KEY `fk_magv_nhomtt` (`MaGV`),
-  ADD KEY `fk_masv_nhomtt` (`MaSV`);
-
---
--- Indexes for table `sinhvien`
---
-ALTER TABLE `sinhvien`
-  ADD PRIMARY KEY (`MaSV`),
-  ADD KEY `fk_malop` (`MaLop`);
-
---
--- Indexes for table `thongtindt`
---
-ALTER TABLE `thongtindt`
-  ADD PRIMARY KEY (`MaDT`),
-  ADD KEY `fk_masv_thongtindt` (`MaSV`),
-  ADD KEY `fk_magv_thongtindt` (`MaGV`);
-
---
--- Constraints for dumped tables
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- Constraints for table `bangdiemtt`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `bangdiemtt`
-  ADD CONSTRAINT `fk_madt_bangdiemtt` FOREIGN KEY (`MaDT`) REFERENCES `detai` (`MaDT`),
-  ADD CONSTRAINT `fk_masv_bangdiemtt` FOREIGN KEY (`MaSV`) REFERENCES `sinhvien` (`MaSV`);
-
---
--- Constraints for table `lop`
---
-ALTER TABLE `lop`
-  ADD CONSTRAINT `fk_makh` FOREIGN KEY (`MaKH`) REFERENCES `khoahoc` (`MaKH`);
-
---
--- Constraints for table `nhomtt`
---
-ALTER TABLE `nhomtt`
-  ADD CONSTRAINT `fk_magv_nhomtt` FOREIGN KEY (`MaGV`) REFERENCES `giangvienhdtt` (`MaGV`),
-  ADD CONSTRAINT `fk_masv_nhomtt` FOREIGN KEY (`MaSV`) REFERENCES `sinhvien` (`MaSV`);
-
---
--- Constraints for table `sinhvien`
---
-ALTER TABLE `sinhvien`
-  ADD CONSTRAINT `fk_malop` FOREIGN KEY (`MaLop`) REFERENCES `lop` (`MaLop`),
-  ADD CONSTRAINT `fk_masv_sinhvien` FOREIGN KEY (`MaSV`) REFERENCES `account` (`username`);
-
---
--- Constraints for table `thongtindt`
---
-ALTER TABLE `thongtindt`
-  ADD CONSTRAINT `fk_magv_thongtindt` FOREIGN KEY (`MaGV`) REFERENCES `giangvienhdtt` (`MaGV`),
-  ADD CONSTRAINT `fk_masv_thongtindt` FOREIGN KEY (`MaSV`) REFERENCES `sinhvien` (`MaSV`);
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
