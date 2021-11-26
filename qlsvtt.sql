0584f6863a59243f714830a25f2969e8a3d54416
mocvu1720 <nguyenhuuhuy170820@gmail.com>
2021-11-26 21:14:58 +0700

    change db

--- qlsvtt.sql
+++ qlsvtt.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 25, 2021 at 06:32 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.25
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 26, 2021 lúc 03:13 PM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `qlsvtt`
+-- Cơ sở dữ liệu: `qlsvtt`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bangdiemtt`
+-- Cấu trúc bảng cho bảng `bangdiemtt`
 --
 
 CREATE TABLE `bangdiemtt` (
@@ -37,32 +37,46 @@ CREATE TABLE `bangdiemtt` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detai`
+-- Cấu trúc bảng cho bảng `topics`
 --
 
-CREATE TABLE `detai` (
-  `MaDT` char(10) NOT NULL,
-  `TenDt` varchar(50) NOT NULL,
-  `MaGV` char(10) NOT NULL COMMENT 'Mã giáo viên',
-  `LoaihinhTT` varchar(20) NOT NULL COMMENT 'Loại hình thực tập'
+CREATE TABLE `topics` (
+  `id` int(11) NOT NULL,
+  `topic` varchar(30) NOT NULL,
+  `user_id` varchar(60) NOT NULL,
+  `topic_id` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nhomtt`
+-- Cấu trúc bảng cho bảng `topic_lecturers`
 --
 
-CREATE TABLE `nhomtt` (
-  `MaDT` char(10) NOT NULL,
-  `MaGV` char(10) NOT NULL,
-  `MaSV` char(10) NOT NULL
+CREATE TABLE `topic_lecturers` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(60) NOT NULL,
+  `topic_id` varchar(60) NOT NULL,
+  `disabled` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `topic_students`
+--
+
+CREATE TABLE `topic_students` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(60) NOT NULL,
+  `topic_id` varchar(60) NOT NULL,
+  `disabled` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
@@ -79,31 +93,51 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Đang đổ dữ liệu cho bảng `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `date`, `user_id`, `gender`, `rank`, `password`, `image`) VALUES
-(1, 'Eathorne', 'Banda', 'eathorne@yahoo.com', '2021-08-10 19:08:58', 'eathorne.banda', 'male', 'super_admin', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', 'uploads/cardinal_1585485603.jpg'),
+(1, 'Eathorne', 'Banda', 'eathorne@yahoo.com', '2021-08-10 19:08:58', 'eathorne.banda', 'male', 'admin', '$2y$10$rh8FOWOtj/OmD75/4R5riOsU0HlrOa.RnO75ZdJx2w8AoPDH6tyrG', 'uploads/cardinal_1585485603.jpg'),
 (3, 'John', 'Tembo', 'john@yahoo.com', '2021-08-18 14:43:04', 'john.tembo', 'male', 'admin', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', 'uploads/header100people.jpg'),
-(4, 'Anna', 'Jones', 'anna@yahoo.com', '2021-08-18 15:02:29', 'anna.jonnes', 'female', 'reception', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
-(5, 'Vibe', 'Peters', 'vibe@yahoo.com', '2021-08-18 15:03:07', 'vibe.peters', 'male', 'lecturer', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
-(6, 'Bob', 'Marley', 'bob@yahoo.com', '2021-08-18 16:03:55', 'bob.marley', 'male', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
-(7, 'Maria', 'Jonnes', 'maria@yahoo.com', '2021-08-18 16:06:27', 'maria.jonnes', 'female', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
-(8, 'Jane', 'Mandawa', 'jane@yahoo.com', '2021-08-18 16:07:00', 'jane.mandawa', 'female', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
-(9, 'Guy', 'Dude', 'guy@yahoo.com', '2021-08-21 18:26:48', 'guy.dude', 'male', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', '');
+(4, 'Vibe', 'Peters', 'vibe@yahoo.com', '2021-08-18 15:03:07', 'vibe.peters', 'male', 'lecturer', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
+(5, 'Bob', 'Marley', 'bob@yahoo.com', '2021-08-18 16:03:55', 'bob.marley', 'male', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
+(6, 'Maria', 'Jonnes', 'maria@yahoo.com', '2021-08-18 16:06:27', 'maria.jonnes', 'female', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
+(7, 'Jane', 'Mandawa', 'jane@yahoo.com', '2021-08-18 16:07:00', 'jane.mandawa', 'female', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
+(8, 'Guy', 'Dude', 'guy@yahoo.com', '2021-08-21 18:26:48', 'guy.dude', 'male', 'student', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', '');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `detai`
+-- Chỉ mục cho bảng `topics`
 --
-ALTER TABLE `detai`
-  ADD PRIMARY KEY (`MaDT`);
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `topic` (`topic`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `topic_id` (`topic_id`);
 
 --
--- Indexes for table `users`
+-- Chỉ mục cho bảng `topic_lecturers`
+--
+ALTER TABLE `topic_lecturers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `disabled` (`disabled`),
+  ADD KEY `topic_id` (`topic_id`);
+
+--
+-- Chỉ mục cho bảng `topic_students`
+--
+ALTER TABLE `topic_students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `disabled` (`disabled`),
+  ADD KEY `topic_id` (`topic_id`);
+
+--
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -116,11 +150,29 @@ ALTER TABLE `users`
   ADD KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `topic_lecturers`
+--
+ALTER TABLE `topic_lecturers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `topic_students`
+--
+ALTER TABLE `topic_students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
