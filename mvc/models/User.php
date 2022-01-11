ecafba1b328ea593c7722783bdb45ff877854557
Van Tien <tien.tien9264@gmail.com>
2022-01-11 11:18:57 +0700

    Update Csignupstu Vsignupstu

--- mvc/models/User.php
+++ mvc/models/User.php
@@ -13,7 +13,7 @@ class User extends Model
         'password',
         'gender',
         'rank',
-        'birthday',
+        'date',
     ];
 
     protected $beforeInsert = [
@@ -26,73 +26,63 @@ class User extends Model
     ];
 
 
-    public function validate($DATA,$id = '')
+    public function validate($DATA, $id = '')
     {
         $this->errors = array();
 
         //check for first name
-        if(empty($DATA['firstname']) || !preg_match('/^[a-zA-Z]+$/', $DATA['firstname']))
-        {
+        if (empty($DATA['firstname']) || !preg_match('/^[a-zA-Z]+$/', $DATA['firstname'])) {
             $this->errors['firstname'] = "Only letters allowed in first name";
         }
 
         //check for last name
-        if(empty($DATA['lastname']) || !preg_match('/^[a-zA-Z]+$/', $DATA['lastname']))
-        {
+        if (empty($DATA['lastname']) || !preg_match('/^[a-zA-Z]+$/', $DATA['lastname'])) {
             $this->errors['lastname'] = "Only letters allowed in last name";
         }
 
         //check for email
-        if(empty($DATA['email']) || !filter_var($DATA['email'],FILTER_VALIDATE_EMAIL))
-        {
+        if (empty($DATA['email']) || !filter_var($DATA['email'], FILTER_VALIDATE_EMAIL)) {
             $this->errors['email'] = "Email is not valid";
         }
 
         //check if email exists
-        if(trim($id) == ""){
-            if($this->where('email',$DATA['email']))
-            {
+        if (trim($id) == "") {
+            if ($this->where('email', $DATA['email'])) {
                 $this->errors['email'] = "That email is already in use";
             }
-        }else{
-            if($this->query("select email from $this->table where email = :email && user_id != :id",['email'=>$DATA['email'],'id'=>$id]))
-            {
+        } else {
+            if ($this->query("select email from $this->table where email = :email && user_id != :id", ['email' => $DATA['email'], 'id' => $id])) {
                 $this->errors['email'] = "That email is already in use";
             }
         }
 
         //check for password
-        if(isset($DATA['password'])){
+        if (isset($DATA['password'])) {
 
-            if(empty($DATA['password']) || $DATA['password'] !== $DATA['password2'])
-            {
+            if (empty($DATA['password']) || $DATA['password'] !== $DATA['password2']) {
                 $this->errors['password'] = "Passwords do not match";
             }
 
             //check for password length
-            if(strlen($DATA['password']) < 8)
-            {
+            if (strlen($DATA['password']) < 8) {
                 $this->errors['password'] = "Password must be at least 8 characters long";
             }
         }
 
         //check for gender
-        $genders = ['female','male'];
-        if(empty($DATA['gender']) || !in_array($DATA['gender'], $genders))
-        {
+        $genders = ['female', 'male'];
+        if (empty($DATA['gender']) || !in_array($DATA['gender'], $genders)) {
             $this->errors['gender'] = "Gender is not valid";
         }
 
         //check for gender
-        $ranks = ['student','lecturer','admin'];
-        if(empty($DATA['rank']) || !in_array($DATA['rank'], $ranks))
-        {
+        $ranks = ['student', 'lecturer', 'admin'];
+        if (empty($DATA['rank']) || !in_array($DATA['rank'], $ranks)) {
             $this->errors['rank'] = "Rank is not valid";
         }
 
 
-        if(count($this->errors) == 0)
-        {
+        if (count($this->errors) == 0) {
             return true;
         }
 
@@ -103,9 +93,8 @@ class User extends Model
     {
         $data['user_id'] = strtolower($data['firstname'] . "." . $data['lastname']);
 
-        while($this->where('user_id',$data['user_id']))
-        {
-            $data['user_id'] .= rand(10,9999);
+        while ($this->where('user_id', $data['user_id'])) {
+            $data['user_id'] .= rand(10, 9999);
         }
 
         return $data;
@@ -113,12 +102,10 @@ class User extends Model
 
     public function hash_password($data)
     {
-        if(isset($data['password'])){
+        if (isset($data['password'])) {
             $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
         }
 
         return $data;
     }
-
-
 }
