2abc4ee15fb0c950160268167f165f7d1c5427b8
mocvu1720 <nguyenhuuhuy170820@gmail.com>
2022-02-22 16:05:13 +0700

    Update User.php

--- mvc/models/User.php
+++ mvc/models/User.php
@@ -98,7 +98,7 @@ class User extends Model
 
     public function make_user_id($data)
     {
-        $data['user_id'] = strtolower($data['firstname'] . "." . $data['lastname']);
+        $data['user_id'] = strtolower($data['firstname'] . "." . trim($data['lastname']));
 
         while ($this->where('user_id', $data['user_id'])) {
             $data['user_id'] .= rand(10, 9999);
