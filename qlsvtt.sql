6408d6d3fcb3630b6d7d868dff1c1b31036c7704
Nghia <emailsub789@gmail.com>
2022-03-18 01:18:29 +0700

    update db

--- qlsvtt.sql
+++ qlsvtt.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 17, 2022 lúc 01:08 PM
--- Phiên bản máy phục vụ: 10.4.22-MariaDB
--- Phiên bản PHP: 8.1.1
+-- Host: 127.0.0.1
+-- Generation Time: Mar 17, 2022 at 07:17 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `qlsvtt`
+-- Database: `qlsvtt`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `scores`
+-- Table structure for table `scores`
 --
 
 CREATE TABLE `scores` (
@@ -35,11 +35,12 @@ CREATE TABLE `scores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `scores`
+-- Dumping data for table `scores`
 --
 
 INSERT INTO `scores` (`topic_id`, `user_id`, `score`, `notes`) VALUES
-('', 'maitruong.giang', 0, ''),
+('', 'l??.ngh??a', 0, ''),
+('DT05', 'maitruong.giang', 8, ''),
 ('', 'nguyen.hai', 0, ''),
 ('', 'nguyen.nhat', 0, ''),
 ('', 'nguyen.vy', 0, ''),
@@ -57,7 +58,7 @@ INSERT INTO `scores` (`topic_id`, `user_id`, `score`, `notes`) VALUES
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `topics`
+-- Table structure for table `topics`
 --
 
 CREATE TABLE `topics` (
@@ -72,7 +73,7 @@ CREATE TABLE `topics` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `topics`
+-- Dumping data for table `topics`
 --
 
 INSERT INTO `topics` (`id`, `topic`, `user_id`, `topic_id`, `create_date`, `date_submit`, `members`, `disabled`) VALUES
@@ -86,7 +87,7 @@ INSERT INTO `topics` (`id`, `topic`, `user_id`, `topic_id`, `create_date`, `date
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `topic_students`
+-- Table structure for table `topic_students`
 --
 
 CREATE TABLE `topic_students` (
@@ -96,10 +97,17 @@ CREATE TABLE `topic_students` (
   `disabled` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `topic_students`
+--
+
+INSERT INTO `topic_students` (`id`, `user_id`, `topic_id`, `disabled`) VALUES
+(96, 'maitruong.giang', 'DT01', 0);
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -116,18 +124,18 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Đang đổ dữ liệu cho bảng `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `date`, `user_id`, `gender`, `rank`, `password`, `image`) VALUES
 (1, 'Trọng', 'Hoàng', 'tronghoang@gmail.com', '2021-08-10 19:08:58', 'trong.hoang', 'male', 'admin', '$2y$10$XPYp4iXx2tHqkiphe1D.fepUw.TW1PYjYDkMGMY0p4MDLcvCaEZoS', ''),
-(3, 'Phát', 'Đạt', 'admin@gmail.com', '2021-08-18 14:43:04', 'phat.dat', 'male', 'admin', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', ''),
+(3, 'Phát', 'Đạt', 'admin@gmail.com', '2021-08-18 14:43:04', 'phat.dat', 'male', 'admin', '$2y$10$M7pc9tdHmCmHGgbhhl3zvOER1ozNSYetE6Gv23Yim0GhE2XFqvwgS', 'uploads/1647540818_images.jpg'),
 (24, 'Trần', 'Anh', 'lecturer@gmail.com', '2022-02-23 04:36:05', 'tran.anh', 'male', 'lecturer', '$2y$10$i.HnYMw5yFbuSzVvNjYQvuGUUPIO2eipO5yzyu2Jz.RyCvNmC6yyi', ''),
 (25, 'Hữu', 'Nghĩa', 'huunghia@gmail.com', '2022-02-23 04:37:24', 'huu.nghia', 'male', 'lecturer', '$2y$10$q7isPap4r5fqecVeFXt72.Q6oqb6w0f.mKi8v3ZStwboU5y6v29.G', ''),
-(26, 'Nguyễn', 'Tiên', 'nguyentien@gmail.com', '2022-02-23 04:38:31', 'nguyen.tien', 'female', 'lecturer', '$2y$10$Jjo2reLNg1W4UUlh6pun9erBVzE6xvTfFdj5XKu8ObWBsF9IeJoWG', ''),
-(27, 'Phạm', 'Tuấn', 'phamtuan@gmail.com', '2022-02-23 04:40:05', 'pham.tuan', 'male', 'lecturer', '$2y$10$bfsm18Htfmom39aQVE6EIO8w2DTuJqxcIWYuZatMncI416VW6O2E.', ''),
+(26, 'Nguyễn', 'Tiên', 'nguyentien@gmail.com', '2022-02-23 04:38:31', 'nguyen.tien', 'female', 'lecturer', '$2y$10$Jjo2reLNg1W4UUlh6pun9erBVzE6xvTfFdj5XKu8ObWBsF9IeJoWG', 'uploads/1647540938_download.jpg'),
+(27, 'Phạm', 'Tuấn', 'phamtuan@gmail.com', '2022-02-23 04:40:05', 'pham.tuan', 'male', 'lecturer', '$2y$10$bfsm18Htfmom39aQVE6EIO8w2DTuJqxcIWYuZatMncI416VW6O2E.', 'uploads/1647540924_images (2).jpg'),
 (28, 'Yến', 'Trâm', 'yentram@gmail.com', '2022-02-23 04:40:43', 'yen.tram', 'female', 'lecturer', '$2y$10$GyZmk1iMuBQciJXezdRoRuhWaqbxMvKKp33vhLwbY7d3oNT0hCEXm', ''),
-(29, 'Nguyễn', 'Phúc', 'nguyenphuc@gmail.com', '2022-02-23 06:05:29', 'nguyen.phuc', 'male', 'lecturer', '$2y$10$clt.2/VdeE33zyxW3DrNxOQ9PJb3YEIm/rP8mSsQo8UP3TRn8XXy.', ''),
+(29, 'Nguyễn', 'Phúc', 'nguyenphuc@gmail.com', '2022-02-23 06:05:29', 'nguyen.phuc', 'male', 'lecturer', '$2y$10$clt.2/VdeE33zyxW3DrNxOQ9PJb3YEIm/rP8mSsQo8UP3TRn8XXy.', 'uploads/1647540905_images (1).jpg'),
 (30, 'Mai Trường', 'Giang', 'student@gmail.com', '2022-02-23 13:20:35', 'maitruong.giang', 'male', 'student', '$2y$10$76jJG3ChfHEL2nagejNSOubnri.ilNWl1i3m6O6b2e0SdGZI7xZe.', ''),
 (31, 'Nguyễn', 'Hải', 'nguyenhai@gmail.com', '2022-02-23 13:20:57', 'nguyen.hai', 'male', 'student', '$2y$10$GHQ1jDlMDr4XtWs8RNUwDu/NMcFGbZOIs4HYosxpp7UOGz06Oi2T.', ''),
 (32, 'Phạm Hữu', 'Nam', 'phamhnam@gmail.com', '2022-02-23 13:21:24', 'phamhuu.nam', 'male', 'student', '$2y$10$96ByOZXHBcDG3zomBMb8a.qBtgts4FFWRVTA8Gnn8StlxyeXJEbMy', ''),
@@ -139,22 +147,22 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `date`, `user_id`, 
 (38, 'Nguyễn', 'Vy', 'nguyenvy@gmail.com', '2022-02-23 13:23:44', 'nguyen.vy', 'female', 'student', '$2y$10$/7JpdE6AURTSdmVwlWH0cuueW3Ig7F06ggaWNoUlUP9x49RDQf83O', ''),
 (39, 'Phạm Kim', 'Tuyến', 'phamtuyen@gmail.com', '2022-02-23 13:24:17', 'phamkim.tuyen', 'female', 'student', '$2y$10$B5cK23bvJtTliYBpkSo2ie4KpFuK3EsfqR/FT4bIwUrvgOSbcV3he', ''),
 (40, 'Nguyễn Hồng', 'Trà', 'hongtra@gmail.com', '2022-02-23 13:24:55', 'nguyenhong.tra', 'male', 'student', '$2y$10$I5rdhuj8oN2PmfYNSfDUXOjP5l4YXz.kMGrGlKyHCFVkAjceTVgm6', ''),
-(41, 'Nguyễn Minh', 'Nhi', 'minhnhi@gmail.com', '2022-02-23 13:25:12', 'nguyenminh.nhi', 'female', 'student', '$2y$10$BTQF6GJLXS8lCQ1K/5ZCo.00bHsqRkgUPy5p12QGGctRXZZm.3NCK', ''),
-(42, 'Phạm Minh', 'Anh', 'phamanh@gmail.com', '2022-02-23 13:37:14', 'phamminh.anh', 'female', 'student', '$2y$10$S/LHc9XSRY9uU24eCcGtseTEgeaRSWJn6Ug9HCW40feIDsRdbJHBi', ''),
-(43, 'Trần Văn', 'An', 'tranan@gmail.com', '2022-02-23 13:37:48', 'tranvan.an', 'male', 'student', '$2y$10$XRxTU6Hd68NsliHbd9gtTuJn0KVysqmOB.HXDQmUoUr9l4nolqWNO', '');
+(41, 'Nguyễn Minh', 'Nhi', 'minhnhi@gmail.com', '2022-02-23 13:25:12', 'nguyenminh.nhi', 'female', 'student', '$2y$10$BTQF6GJLXS8lCQ1K/5ZCo.00bHsqRkgUPy5p12QGGctRXZZm.3NCK', 'uploads/1647540889_images (3).jpg'),
+(42, 'Phạm Minh', 'Anh', 'phamanh@gmail.com', '2022-02-23 13:37:14', 'phamminh.anh', 'female', 'student', '$2y$10$S/LHc9XSRY9uU24eCcGtseTEgeaRSWJn6Ug9HCW40feIDsRdbJHBi', 'uploads/1647540847_download.jpg'),
+(43, 'Trần Văn', 'An', 'tranan@gmail.com', '2022-02-23 13:37:48', 'tranvan.an', 'male', 'student', '$2y$10$XRxTU6Hd68NsliHbd9gtTuJn0KVysqmOB.HXDQmUoUr9l4nolqWNO', 'uploads/1647540871_download (1).jpg');
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `scores`
+-- Indexes for table `scores`
 --
 ALTER TABLE `scores`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Chỉ mục cho bảng `topics`
+-- Indexes for table `topics`
 --
 ALTER TABLE `topics`
   ADD PRIMARY KEY (`id`),
@@ -163,7 +171,7 @@ ALTER TABLE `topics`
   ADD KEY `topic_id` (`topic_id`);
 
 --
--- Chỉ mục cho bảng `topic_students`
+-- Indexes for table `topic_students`
 --
 ALTER TABLE `topic_students`
   ADD PRIMARY KEY (`id`),
@@ -172,7 +180,7 @@ ALTER TABLE `topic_students`
   ADD KEY `topic_id` (`topic_id`);
 
 --
--- Chỉ mục cho bảng `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -185,26 +193,26 @@ ALTER TABLE `users`
   ADD KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `topics`
+-- AUTO_INCREMENT for table `topics`
 --
 ALTER TABLE `topics`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT cho bảng `topic_students`
+-- AUTO_INCREMENT for table `topic_students`
 --
 ALTER TABLE `topic_students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
--- AUTO_INCREMENT cho bảng `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
